5eeb3e7877577600c9e226e03a184766ae967ad0
= <=abdoelsimy123@gmail.com>
2019-09-08 17:12:48 +0200

    create models , controllers and migrations plus prepare ll migrations create all tables of the project

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('email_verified')->default(false);
+            $table->string('mobile')->nullable();
+            $table->boolean('mobile_verified')->default(false);
             $table->string('password');
+            $table->bigInteger('shipping_address');
+            $table->bigInteger('billing_address');
             $table->rememberToken();
             $table->timestamps();
         });
